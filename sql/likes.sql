6417091a97e9d88b4cec91800042cf6ea4f70e90
kellan <kellan@pobox.com>
2018-08-02 19:21:37 -0400

    schema

--- sql/likes.sql
+++ sql/likes.sql
@@ -1,17 +1,19 @@
-CREATE TABLE likes (
-	id numeric,
-	source varchar(80),
-	created_at timestamp,
-	inserted_at timestamp,
-	updated_at timestamp,
-	data jsonb
-);
+-- CREATE TABLE likes (
+-- 	id numeric,
+-- 	source varchar(80),
+-- 	created_at timestamp,
+-- 	inserted_at timestamp,
+-- 	updated_at timestamp,
+-- 	data jsonb
+-- );
 
-create unique index on likes (id);
+-- create unique index on likes (id);
 
 -- insert into likes (id, source, created_at, inserted_at, data)
 -- values 
 -- ('966744545936453632', 'tweet', 'Thu Feb 22 18:40:35 +0000 2018', now())
+
+-- SQLITE schema
 
 CREATE TABLE likes (
 	id numeric,
@@ -24,3 +26,5 @@ CREATE TABLE likes (
 	urls text,
 	data text
 );
+
+CREATE UNIQUE INDEX likes_idx on likes (id);
